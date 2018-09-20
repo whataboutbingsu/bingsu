bcb024157e0f85295a5b1d04e19667ab29911390
whataboutbingsu <43279803+whataboutbingsu@users.noreply.github.com>
2018-09-20 16:00:25 -0700

    Update main.cpp

--- Graph Visualizer/main.cpp
+++ Graph Visualizer/main.cpp
@@ -1,28 +1,29 @@
 #include "SimpleGraph.hpp"
 #include <string>
 #include <ctime>
-#include <fstream>
-
+//#include <fstream>
+//using std::string;
+//using std::cout;
+//using std::cin;
 using namespace std;
 
 int main() {
+	
+	SimpleGraph SG;
+	DrawGraph(SG);
+	time_t startTime = time(NULL);
+	//double elapsedTime = difftime(time(NULL), startTime);
 
-    SimpleGraph SG;
-    DrawGraph(SG);
-/**
-    time_t startTime = time(NULL);
-//    double elapsedTime = difftime(time(NULL), startTime);
-
-    /* ... some complex operation ...
-        do
-        {
-            SG.update();
-            // draw every now and again.
-            cout << "\ntime so far = "
-                 << difftime(time(NULL), startTime);
-        } while (difftime(time(NULL), startTime) < 10);
-**/
-    cin.ignore();
-    cin.get();
-
+	/* ... some complex operation ... */
+	do {
+		SG.update();
+		SG.printDistances();
+		cin.get();
+		// draw every now and again.
+		//cout << "\ntime so far = " << difftime(time(NULL), startTime);
+	} while (difftime(time(NULL), startTime) < 10);
+	// draw again at the end
+	cin.ignore();
+	cin.get();
+	
 }
